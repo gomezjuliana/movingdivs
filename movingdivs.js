222064668be9728788df4aa59487630b03402f1b
Jules <jgmz.jg@gmail.com>
2017-04-07 18:39:33 -0500

    improve function

--- movingdivs.js
+++ movingdivs.js
@@ -1,13 +1,4 @@
-var toggle = true;
-
 function changeHeight(){
-	if (toggle === true){
-	document.querySelector('.container').classList.add('smaller');
-	console.log(toggle);
-	toggle = false;
-	console.log(toggle);
-    } else {
-    	document.querySelector('.container').classList.remove('smaller');
-    	toggle = true;
-    }
+	document.querySelector('.container').classList.toggle('smaller');
+	document.querySelector('.container').classList.toggle('bigger');
 }
